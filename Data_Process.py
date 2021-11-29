44a718b4dd6b0b5bc7084fcd6db22650fc92627c
alien-traveler <jiwenwillluo@gmail.com>
2021-11-29 15:14:11 -0800

    11/29/21 filter date

---
+++ Data_Process.py
@@ -0,0 +1,24 @@
+import csv
+
+with open('California_Fire_Perimeters_(all).csv') as read_file:
+    with open('result.csv', mode='w', newline='') as write_file:
+        read_file = csv.reader(read_file, delimiter=',')
+        write_file = csv.writer(write_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        line_count = 0
+        # read through data
+        for row in read_file:
+            # column row
+            if line_count == 0:
+                # print(f'Column names are {", ".join(row)}')
+                write_file.writerow(row)
+                line_count += 1
+            # all other rows
+            else:
+                # if the year is between 2018 - 2020, write that row of data into the new file
+                if(row[1] == '2020' or row[1] == '2019' or row[1] == '2018'):
+                    # print(row)
+                    write_file.writerow(row)
+                    line_count += 1
+
+        print(f'Processed {line_count} lines.')
+
